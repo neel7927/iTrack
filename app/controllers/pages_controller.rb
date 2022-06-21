7129aa3a69813112dace0bdd847d15b6a9e63180
neelesh <neel7927@gmail.com>
2022-06-21 17:05:34 +0100

    changes to controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    if user_signed_in?
-      redirect_to accounts_path
-    end
+
   end
 end
