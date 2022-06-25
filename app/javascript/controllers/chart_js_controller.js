9c0d33821cb65fc3ff592450880486a548fe68c8
Sajjaad <sajjaadsahodeea@gmail.com>
2022-06-25 12:53:32 +0400

    Modified chart, added tabs

--- app/javascript/controllers/chart_js_controller.js
+++ app/javascript/controllers/chart_js_controller.js
@@ -3,20 +3,36 @@ import Chart from 'chart.js/auto';
 
 export default class extends Controller {
   connect() {
-    new Chart(this.element, {
+    const ctx = this.element.getContext("2d");
+    new Chart(ctx, {
       type: 'doughnut',
       data: {
-        labels: JSON.parse(this.element.canvas.dataset.labels),
+        labels: JSON.parse(ctx.canvas.dataset.labels),
         datasets: [{
           label: 'Total Expenses',
-          data: JSON.parse(this.element.canvas.dataset.data),
+          data: JSON.parse(ctx.canvas.dataset.data),
           backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
+            'rgba(255, 99, 132, 0.6)',
+            'rgba(54, 162, 235, 0.6)',
+            'rgba(255, 206, 86, 0.6)',
+            'rgba(75, 192, 192, 0.6)',
+            'rgba(153, 102, 255, 0.6)',
+            'rgba(255, 159, 64, 0.6)'
           ],
           hoverOffset: 4
         }]
+      },
+      options: {
+        elements: {
+          center: {
+            text: `${JSON.parse(ctx.canvas.dataset.text)}`,
+            color: 'rgb(255, 99, 132)', // Default is #000000
+            fontStyle: 'Arial', // Default is Arial
+            sidePadding: 20, // Default is 20 (as a percentage)
+            minFontSize: 20, // Default is 20 (in px), set to false and text will not wrap.
+            lineHeight: 25 // Default is 25 (in px), used for when text wraps
+          }
+        }
       }
     });
   }
