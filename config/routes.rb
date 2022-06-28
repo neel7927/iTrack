b17ee0cc0fa4fb34f66d962d124a776c66bee7e6
ibrahim <ibrahim.peerbux28@gmail.com>
2022-06-28 23:31:36 +0400

    transactions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :accounts, only: [:index, :show, :new, :create, :destroy] do
-    resources :transactions, only: [:index, :show]
+    resources :transactions, only: [:index, :show, :edit]
     resources :budgets, only: [:new, :create, :show, :index]
   end
   resources :categories, only: [:index, :show, :edit, :update]
