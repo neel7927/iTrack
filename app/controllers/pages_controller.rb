36f1b03c76e963431f7d828003f184fec7272916
neelesh <neel7927@gmail.com>
2022-06-30 18:08:57 +0100

    changes to user signin

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,11 @@ class PagesController < ApplicationController
 
   def home
     if user_signed_in?
-      redirect_to accounts_path
+      if current_user.account.present?
+        redirect_to accounts_path
+        else
+        redirect_to new_account_path
+      end
     end
   end
 end
