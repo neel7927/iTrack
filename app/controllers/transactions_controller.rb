94e793696b001e30a206125a5a4b9f2ba2e6bee9
Mun Kadell <kadellppsay@gmail.com>
2022-07-02 11:16:12 +0400

    footer fixed + created whole search page

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,9 @@
 class TransactionsController < ApplicationController
+  def index
+    if params[:query].present?
+      @transactions = Transaction.where("name ILIKE ?", "%#{params[:query]}%")
+    else
+      @transactions = Transaction.all
+    end
+  end
 end
---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dvyjgegyp'
+  config.api_key = '229962191631512'
+  config.api_secret = '-aMSsQ-1uxYb9waaH734De9vMmg'
+  config.secure = true
+end
